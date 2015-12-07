bf342a3383e5827968496e84a7ec306286a853d2
Eric <ejh81491@gmail.com>
2015-12-07 17:15:59 -0500

    updated look

--- public/main.js
+++ public/main.js
@@ -3,9 +3,11 @@
 // 		"background-color", "yellow"
 // 		);
 // })
+// 
+var Status = "Boutique";
 
 $(document).on("ready", function(){
-var Status = "Boutique";
+
 
 	$('.div1').on("click", function(){
 		if(Status === "Boutique"){
@@ -15,6 +17,8 @@ var Status = "Boutique";
 			$('.div2').css(
 				"background-color", "#4A4A4C"
 				);
+			$('.table3').hide()
+			$('.div3').css("padding", "20px")
 			$('.seven').text('Studio');
 			$('.ten').text('Boutique');
 			$('.table2').hide();
@@ -27,6 +31,8 @@ var Status = "Boutique";
 			$('.div2').css(
 				"background-color", "#808285"
 				);
+			$('.table3').hide()
+			$('.div3').css("padding", "20px")
 			$('.seven').text('Boutique');
 			$('.ten').text('Studio');
 			$('.table2').show();
@@ -37,16 +43,25 @@ var Status = "Boutique";
 	})
 $('.work1').on('click', function(e){
 	e.stopPropagation();
-	$('.table2').slideUp();
+	$('.table2').hide();
 
-	$('.div3').css("padding-left", "260px").append('<table><tr><td><img src="1.jpg" style="width:512px;height:380px"></td></tr><tr><td><img src="1.jpg" style="width:512px;height:380px"></td></tr><tr><td><img src="1.jpg" style="width:512px;height:380px"></td></tr><tr><td><img src="1.jpg" style="width:512px;height:380px"></td></tr></table>');
+	$('.div3').css("padding-left", "260px").append('<table class="table3"><tr><td><img src="assets/1.jpg" style="width:512px;height:380px"></td></tr><tr><td><img src="assets/1.jpg" style="width:512px;height:380px"></td></tr></table>');
 		
 
 	
 })
 	
-
+$('.ten, .one').on('click', function(){
+	if(Status ==="Studio"){
+		console.log('do nothing')
+	}else{
+		$('.div3').css("padding", "20px")
+	$('.table2').show();
+	$('.table3').hide();
+	}
+	
 		
+	})
 })
 
 
